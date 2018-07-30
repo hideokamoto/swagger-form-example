cecd037d3630d54ac3eaa98fe3cb9293abc71a3e
hideokamoto <hide.okamoto@digitalcube.jp>
2018-07-30 16:06:52 +0900

    init dynamic rouitng

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch, Link, withRouter } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 const file = require('./swagger/sample.yml')
@@ -7,59 +8,97 @@ const SwaggerParser = require('swagger-parser')
 
 const genReactRouterPath = (basePath, path) => {
   if (!/\{/.test(path)) return `${basePath}${path}`
-  const newPath = path.replace(/\{/g, '{:')
-  const base = path.replace(/\{/g, '{:')
-  console.log(base)
-  return `${base}${newPath}`
+  return `${basePath}${path}`.replace(/\{/g, ':').replace(/\}/g, '')
 }
+
+const DynamicComponent = withRouter(({path, routes}) => {
+  const route = routes[path]
+  return (
+    <div>
+      <h3>{path}</h3>
+      {Object.keys(route).map((method, key) => {
+        const item = route[method]
+        console.log(item)
+        return (
+          <div key={key}>
+            <h4>Method: {method}</h4>
+            <p>{item.description}</p>
+          </div>
+        )
+      })}
+    </div>
+  )
+})
+
+const Routes = ({routerPaths, routes}) => {
+  return (
+    <Switch>
+      {routerPaths.reverse().map((path, key) => (
+        <Route
+          key={key}
+          path={path}
+          component={() => <DynamicComponent path={path} routes={routes} />}
+        />
+      ))}
+      <Route path="/" exact component={() => <div>Home</div>}/>
+    </Switch>
+  )
+}
+
 
 class App extends Component {
   state = {
     routerPaths: [],
-    navigationPaths: []
+    navigationPaths: [],
+    routes: {},
+    info: {}
   }
   componentDidMount = async () => {
     const data = await SwaggerParser.dereference(file)
-    Object.keys(data.info).forEach(key => console.log(`${data.info[key]}\n`))
-    const { basePath, paths } = data
+    const { basePath, paths, info } = data
     const routerPaths = []
     const navigationPaths = []
-
+    const routes = {}
     Object.keys(paths).forEach(key => {
+      const pathKey = `${basePath}${key}`.replace(/\{/g, '').replace(/\}/g, '')
       const routePath = genReactRouterPath(basePath, key)
-      const routes = paths[key]
-      navigationPaths.push(`${basePath}${key}`.replace(/\{/g, '').replace(/\}/g, ''))
+      routes[routePath] = paths[key]
+      navigationPaths.push(pathKey)
       routerPaths.push(routePath)
     })
-    this.setState({routerPaths, navigationPaths})
+    this.setState({routerPaths, navigationPaths, routes, info})
     return
   }
   render() {
+    const { info, navigationPaths, routerPaths } = this.state;
+    console.log(this.state.routes)
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <div className="router-list">
-          <h2>Links</h2>
-          <ul>
-            {this.state.navigationPaths.map((path, key) => {
-              return <li key={key}>{path}</li>
-            })}
-          </ul>
-
-          <h2>Router</h2>
-          <ul>
-            {this.state.routerPaths.map((path, key) => {
-              return <li key={key}>{path}</li>
-            })}
-          </ul>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <dl className="App-intro">
+            {Object.keys(info).map((term, k) => (
+              <Fragment key={k}>
+                <dt>{term}</dt>
+                <dd>{info[term]}</dd>
+              </Fragment>
+            ))}
+          </dl>
+          <div className="router-list">
+            <h2>Current page</h2>
+            <Routes {...this.state} />
+            <h2>Links</h2>
+            <ul>
+              {navigationPaths.map((path, key) => {
+                return <li key={key}><Link to={path}>{path}</Link></li>
+              })}
+            </ul>
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
